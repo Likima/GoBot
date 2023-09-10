eb85e1ee09516337f88f846a04be594b1a1476f6
Brandon Zenbook <tangbrandonk@gmail.com>
2023-09-10 15:06:19 -0700

    initialized board properly for reversi

--- board.h
+++ board.h
@@ -6,7 +6,7 @@
 struct piece{
     int x;
     int y;
-    int color; // 0 = empty, 1 = black, 2 = white
+    int color = 0; // 0 = empty, 1 = black, 2 = white
     int type;
 };
 
@@ -15,25 +15,40 @@ class PlayingBoard{
         PlayingBoard(){
             std::vector<piece> row;
             piece empty;
-            empty.color = 0;
-            for(int x = 0; x<9; x++){
-                for(int y = 0; y<9; y++){
-                    empty.color = 0;
+            //empty.color;
+            for(int x = 0; x<8; x++){
+                for(int y = 0; y<8; y++){
+                    //if((x == 3 && y == 3) || (x == 4 && y == 4)){
+                    //    empty.color = 2;
+                    //}
+                    //else if((x == 3 && y == 4) || (x == 4 && y == 3)){
+                    //    empty.color = 1;
+                    //}
+                    //else empty.color = 0;
                     empty.x = x;
                     empty.y = y;
                     row.emplace_back(empty);
                 }
                 board.emplace_back(row);
             }
+            placePiece(3,3,2);
+            placePiece(4,4,2);
+            placePiece(3,4,1);
+            placePiece(4,3,1);
         }
+
+        void placePiece(int x, int y, int color){
+            board[x][y].color = color;
+        }
+
         void printBoard(){
-            for(int x = 0; x<9; x++){
-                for(int y = 0; y<9; y++){
+            for(int x = 0; x<8; x++){
+                for(int y = 0; y<8; y++){
                     if(board[x][y].color==0){std::cout<<" . ";}
                     else if(board[x][y].color==1){std::cout<<"\033[0;31m"<<" O "<<"\033[0m";}
                     else std::cout<<"\033[0;36m"<<" O "<<"\033[0m";
                 }
-                std::cout << std::endl;
+                std::cout<<std::endl;
             }
         }
 
--- gobot.cc
+++ gobot.cc
@@ -1,5 +1,7 @@
 #include "board.h"
 
+//realized that this is not go, this is reversi
+
 int main(){
     PlayingBoard board;
     board.printBoard();
